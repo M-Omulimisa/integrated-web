9247cfa08eada9eb35a8db4a27e7b6b5fb500c0a
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-11 10:27:01 +0300

    GET

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::POST("users/login", function () {
     return "Hello World";
 });
 
-Route::get('/user', function (Request $request) {
+Route::GET('/user', function (Request $request) {
     return 'Testing';
 });
 
@@ -31,58 +31,58 @@ Route::group([
 ], function () {
 
     /* ==============START OF SHOP API================== */
-    Route::get('api/{model}', [ApiShopController::class, 'index']);
-    Route::get('products', [ApiShopController::class, 'products']);
+    Route::GET('api/{model}', [ApiShopController::class, 'index']);
+    Route::GET('products', [ApiShopController::class, 'products']);
     Route::POST("product-create", [ApiShopController::class, "product_create"]);
     Route::POST("post-media-upload", [ApiShopController::class, 'upload_media']);
-    Route::post('products-delete', [ApiShopController::class, 'products_delete']);
-    Route::post('chat-send', [ApiShopController::class, 'chat_send']);
-    Route::get('chat-heads', [ApiShopController::class, 'chat_heads']);
-    Route::get('chat-messages', [ApiShopController::class, 'chat_messages']);
-    Route::post('chat-mark-as-read', [ApiShopController::class, 'chat_mark_as_read']);
-    Route::post('chat-start', [ApiShopController::class, 'chat_start']); 
+    Route::POST('products-delete', [ApiShopController::class, 'products_delete']);
+    Route::POST('chat-send', [ApiShopController::class, 'chat_send']);
+    Route::GET('chat-heads', [ApiShopController::class, 'chat_heads']);
+    Route::GET('chat-messages', [ApiShopController::class, 'chat_messages']);
+    Route::POST('chat-mark-as-read', [ApiShopController::class, 'chat_mark_as_read']);
+    Route::POST('chat-start', [ApiShopController::class, 'chat_start']); 
 
     /* ==============END OF SHOP API================== */
 
     Route::put('api/{model}', [ApiShopController::class, 'update']);
     // Authentication
-    Route::post('login', [ApiAuthController::class, 'login']);
-    Route::post('register', [ApiAuthController::class, 'register']);
-    Route::get('me', [ApiAuthController::class, 'me']);
-    Route::get('organisation-joining-requests', [ApiAuthController::class, 'organisation_joining_requests']);
-    Route::get('my-roles', [ApiAuthController::class, 'my_roles']);
-    Route::get('resources', [ApiAuthController::class, 'resources']);
-    Route::get('resources-categories', [ApiAuthController::class, 'resources_categpries']);
-    Route::post('organisation-joining-requests', [ApiAuthController::class, 'organisation_joining_request_post']);
-    Route::get('organisations', [ApiAuthController::class, 'organisations']);
-    Route::post('update-profile', [ApiAuthController::class, 'update_profile']);
-    Route::get('farmer-groups', [ApiAuthController::class, 'farmer_groups']);
-    Route::get('farmers', [ApiAuthController::class, 'farmers']);
-    Route::post('farmers', [ApiAuthController::class, 'farmers_create']);
-    Route::get('countries', [ApiAuthController::class, 'countries']);
-    Route::get('locations', [ApiAuthController::class, 'locations']);
-    Route::get('languages', [ApiAuthController::class, 'languages']);
-    Route::get('trainings', [ApiAuthController::class, 'trainings']);
-    Route::get('farmer-questions', [ApiAuthController::class, 'farmer_questions']);
-    Route::get('farmer_question_answers', [ApiAuthController::class, 'farmer_question_answers']);
-    Route::get('training-sessions', [ApiAuthController::class, 'training_sessions']);
-    Route::post('training-sessions', [ApiAuthController::class, 'training_session_post']);
-    Route::post('farmer-questions-create', [ApiAuthController::class, 'farmer_questions_create']);
-    Route::post('farmer-answers-create', [ApiAuthController::class, 'farmer_answers_create']);
+    Route::POST('login', [ApiAuthController::class, 'login']);
+    Route::POST('register', [ApiAuthController::class, 'register']);
+    Route::GET('me', [ApiAuthController::class, 'me']);
+    Route::GET('organisation-joining-requests', [ApiAuthController::class, 'organisation_joining_requests']);
+    Route::GET('my-roles', [ApiAuthController::class, 'my_roles']);
+    Route::GET('resources', [ApiAuthController::class, 'resources']);
+    Route::GET('resources-categories', [ApiAuthController::class, 'resources_categpries']);
+    Route::POST('organisation-joining-requests', [ApiAuthController::class, 'organisation_joining_request_post']);
+    Route::GET('organisations', [ApiAuthController::class, 'organisations']);
+    Route::POST('update-profile', [ApiAuthController::class, 'update_profile']);
+    Route::GET('farmer-groups', [ApiAuthController::class, 'farmer_groups']);
+    Route::GET('farmers', [ApiAuthController::class, 'farmers']);
+    Route::POST('farmers', [ApiAuthController::class, 'farmers_create']);
+    Route::GET('countries', [ApiAuthController::class, 'countries']);
+    Route::GET('locations', [ApiAuthController::class, 'locations']);
+    Route::GET('languages', [ApiAuthController::class, 'languages']);
+    Route::GET('trainings', [ApiAuthController::class, 'trainings']);
+    Route::GET('farmer-questions', [ApiAuthController::class, 'farmer_questions']);
+    Route::GET('farmer_question_answers', [ApiAuthController::class, 'farmer_question_answers']);
+    Route::GET('training-sessions', [ApiAuthController::class, 'training_sessions']);
+    Route::POST('training-sessions', [ApiAuthController::class, 'training_session_post']);
+    Route::POST('farmer-questions-create', [ApiAuthController::class, 'farmer_questions_create']);
+    Route::POST('farmer-answers-create', [ApiAuthController::class, 'farmer_answers_create']);
 
-    Route::get('districts', [ApiAuthController::class, 'districts']);
-    Route::get('counties', [ApiAuthController::class, 'counties']);
-    Route::get('regions', [ApiAuthController::class, 'regions']);
-    Route::get('subcounties', [ApiAuthController::class, 'subcounties']);
-    Route::get('villages', [ApiAuthController::class, 'villages']);
+    Route::GET('districts', [ApiAuthController::class, 'districts']);
+    Route::GET('counties', [ApiAuthController::class, 'counties']);
+    Route::GET('regions', [ApiAuthController::class, 'regions']);
+    Route::GET('subcounties', [ApiAuthController::class, 'subcounties']);
+    Route::GET('villages', [ApiAuthController::class, 'villages']);
 
     Route::middleware('client_credentials')->group(function () {
-        Route::post('logout', function () {
-            Route::post('logout', [AuthApiController::class, 'logout']);
+        Route::POST('logout', function () {
+            Route::POST('logout', [AuthApiController::class, 'logout']);
         });
     });
 
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Route::middleware('auth:api')->GET('/user', function (Request $request) {
         return $request->user();
     });
 });
@@ -92,7 +92,7 @@ Route::group([
     'prefix' => '/v1'
 ], function () {
 
-    Route::get('ussdmenu', [MenuController::class, 'index']);
+    Route::GET('ussdmenu', [MenuController::class, 'index']);
 });
 
 Route::group([
@@ -102,7 +102,7 @@ Route::group([
 
     Route::middleware('client_credentials', 'passport.client.set')->group(function () {
 
-        // Route::post('request', [ApiController::class, 'method']);
+        // Route::POST('request', [ApiController::class, 'method']);
 
     });
 });
