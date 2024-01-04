728dd77791f0401c70739178584793c7b34d1e0d
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-01-04 11:48:33 +0300

    latest changes

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,24 +3,130 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\AdminRoleUser;
+use App\Models\Organisations\Organisation;
+use App\Models\Product;
+use App\Models\User;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\Box;
+use Faker\Provider\ar_JO\Company;
 
 class HomeController extends Controller
 {
     public function index(Content $content)
     {
         return $content
-            ->title('Dashboard')
+            ->title('M-Omulimisa')
             ->description('Hello...')
-            ->row(Dashboard::title())
+            /* ->row(Dashboard::title()) */
             ->row(function (Row $row) {
-
-/*                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                $row->column(3, function (Column $column) {
+                    $data = [];
+                    $data[] = [
+                        'title' => 'Organisation',
+                        'detail' => Organisation::count(),
+                    ];
+                    $data[] = [
+                        'title' => 'Registered Users',
+                        'detail' => \App\Models\User::count(),
+                    ];
+                    $data[] = [
+                        'title' => 'Extension Officers',
+                        'detail' => AdminRoleUser::where([
+                            'role_id' => 2
+                        ])
+                            ->count(),
+                    ];
+                    $box = new Box(
+                        'System Users',
+                        view('admin.widgets.widget-1', [
+                            'data' => $data,
+                            'url' => route('admin.farmers.index')
+                        ])
+                    );
+                    $link = '<a href="' . route('admin.farmers.index') . '" class="small-box-footer text-success">More info <i class="fa fa-arrow-circle-right"></i></a>';
+                    $box->style('success')
+                        ->footer($link)
+                        ->solid()
+                        ->collapsable()
+                        ->removable();
+                    $column->append($box);
                 });
+
+                $row->column(3, function (Column $column) {
+                    $data = [];
+                    $data[] = [
+                        'title' => 'All Farmers',
+                        'detail' => \App\Models\Farmers\Farmer::count(),
+                    ];
+                    $data[] = [
+                        'title' => 'Farmer Groups',
+                        'detail' => \App\Models\Farmers\FarmerGroup::count(),
+                    ];
+                    $data[] = [
+                        'title' => 'Individual farmers',
+                        'detail' => \App\Models\Farmers\Farmer::where([
+                            'farmer_group_id' => 1
+                        ])
+                            ->count(),
+                    ];
+                    $box = new Box(
+                        'Famers registered',
+                        view('admin.widgets.widget-1', [
+                            'data' => $data,
+                            'url' => route('admin.farmers.index')
+                        ])
+                    );
+                    $link = '<a href="' . route('admin.farmers.index') . '" class="small-box-footer text-success">More info <i class="fa fa-arrow-circle-right"></i></a>';
+                    $box->style('success')
+                        ->footer($link)
+                        ->solid()
+                        ->collapsable()
+                        ->removable();
+                    $column->append($box);
+                });
+
+                $row->column(3, function (Column $column) {
+                    $data = [];
+                    $data[] = [
+                        'title' => 'Products',
+                        'detail' => Product::count(),
+                    ];
+                    $data[] = [
+                        'title' => 'Vendors',
+                        'detail' => User::where([
+                            'role_id' => 3
+                        ])
+                            ->count(),
+                    ];
+                    $data[] = [
+                        'title' => 'Orders',
+                        'detail' => \App\Models\Farmers\Farmer::where([
+                            'farmer_group_id' => 1
+                        ])
+                            ->count(),
+                    ];
+                    $box = new Box(
+                        'Marketplace',
+                        view('admin.widgets.widget-1', [
+                            'data' => $data,
+                            'url' => route('admin.farmers.index')
+                        ])
+                    );
+                    $link = '<a href="' . route('admin.farmers.index') . '" class="small-box-footer text-success">More info <i class="fa fa-arrow-circle-right"></i></a>';
+                    $box->style('success')
+                        ->footer($link)
+                        ->solid()
+                        ->collapsable()
+                        ->removable();
+                    $column->append($box);
+                });
+
+
+                /*                
 
                 $row->column(4, function (Column $column) {
                     $column->append(Dashboard::extensions());
