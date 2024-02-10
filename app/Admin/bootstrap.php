b4caae3d1c5f5d4f4b9e6853c579d4958a16d440
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-02-10 15:43:03 +0300

    Fix typo in status condition

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 
 
-$done_lesson = \App\Models\OnlineCourseLesson::where('student_id', $student->id)
-    ->where('status', 'Pedning')
+$done_lesson = \App\Models\OnlineCourseLesson::where('status', 'Pedning')
     ->update([
         'has_reminder_call' => 'No',
         'reminder_date' => null,
