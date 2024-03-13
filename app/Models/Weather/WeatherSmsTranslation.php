7a8871920ddb8a8855e2310fa33707a2d6e2c4ce
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-03-13 15:12:39 +0300

    Add WeatherSmsTranslationController and language relationship to WeatherSmsTranslation model

--- app/Models/Weather/WeatherSmsTranslation.php
+++ app/Models/Weather/WeatherSmsTranslation.php
@@ -1,15 +1,15 @@
 <?php
 
 namespace App\Models\Weather;
-  
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\BaseModel;
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
-  
+
 class WeatherSmsTranslation extends BaseModel
 {
     use Uuid;
-  
+
     protected $fillable = [
         'language_id',
         'translation',
@@ -43,4 +43,10 @@ class WeatherSmsTranslation extends BaseModel
      * @var bool
      */
     public $incrementing = false;
+
+    //belongs to language
+    public function language()
+    {
+        return $this->belongsTo('App\Models\Settings\Language');
+    }
 }
