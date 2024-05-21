ac6dfee217af83a308a9ef8a5926d7d5e643be32
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-05-21 18:50:11 +0300

    as

--- database/migrations/2024_03_28_152206_adding_map_data_to_the_ussd_session_data_table.php
+++ database/migrations/2024_03_28_152206_adding_map_data_to_the_ussd_session_data_table.php
@@ -14,7 +14,7 @@ class AddingMapDataToTheUssdSessionDataTable extends Migration
     public function up()
     {
         Schema::table('ussd_session_data', function (Blueprint $table) {
-            $table->json('option_mappings')->nullable();
+            $table->text('option_mappings')->nullable();
         });
     }
 
