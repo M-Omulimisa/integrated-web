2c4f6ca11d183e9822bd290d6d898ea0d4fec41c
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-05-21 19:13:41 +0300

    Refactor database migrations to use text instead of json for properties and data fields

--- database/migrations/2024_03_28_152206_adding_map_data_to_the_ussd_session_data_table.php
+++ database/migrations/2024_03_28_152206_adding_map_data_to_the_ussd_session_data_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Utils;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,16 @@ class AddingMapDataToTheUssdSessionDataTable extends Migration
     public function up()
     {
         Schema::table('ussd_session_data', function (Blueprint $table) {
-            $table->text('option_mappings')->nullable();
+            //$table->text('option_mappings')->nullable();
+            Utils::create_column(
+                'ussd_session_data',
+                [
+                    [
+                        'name' => 'option_mappings',
+                        'type' => 'Text',
+                    ],
+                ]
+            );
         });
     }
 
