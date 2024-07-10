5f1a60de87f621ea4ad1bf0837640ead5fa0b5a5
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-07-10 23:55:09 +0300

    Refactor ApiShopController to remove commented out code and update phone number handling

--- app/Http/Controllers/Api/v1/ApiShopController.php
+++ app/Http/Controllers/Api/v1/ApiShopController.php
@@ -396,7 +396,7 @@ class ApiShopController extends Controller
             ]
         );
 
-        $u = auth('api')->user();
+       /*  $u = auth('api')->user();
         if ($u == null) {
             $administrator_id = Utils::get_user_id($r);
             $u = Administrator::find($administrator_id);
@@ -409,7 +409,7 @@ class ApiShopController extends Controller
         if ($phone_number == null || strlen($phone_number) < 3) {
             $phone_number = $u->phone_number;
         }
-
+ */
 
         $phone_number = '+256705128728';
         $phone_number = str_replace('+', '', $phone_number);
