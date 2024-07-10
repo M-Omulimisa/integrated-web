a88c99d252da36255fc31a7c97879592fda710cc
mubiru-simeon <mubzsimeon@gmail.com>
2024-07-10 18:00:06 +0300

    FIxed issues with the names of the fields in handling insurance API Calls

--- app/Http/Controllers/Api/v1/InsuranceAPIController.php
+++ app/Http/Controllers/Api/v1/InsuranceAPIController.php
@@ -66,10 +66,10 @@ class InsuranceAPIController extends Controller
                 "insurance_type" => $r->insurance_type,
                 "surname" => $r->surname,
                 "payment_phone" => $r->payment_phone,
-                "paid" => $this->boolToInt($r->paid),
-                "completed" => $this->boolToInt($r->completed),
-                "pending" => $this->boolToInt($r->pending),
-                "cancelled" => $this->boolToInt($r->cancelled),
+                "paid" => $r->paid,
+                "completed" => $r->completed,
+                "pending" => $r->pending,
+                "cancelled" => $r->cancelled,
                 "national_id" => $r->national_id,
                 "village_id" => $r->village_id,
                 "driving_license" => $r->driving_license,
@@ -110,11 +110,9 @@ class InsuranceAPIController extends Controller
             \App\Models\Ussd\UssdSessionData::create($data);
 
             // Retrieve the session data for the given session ID and phone number.
-            $sessionData = \App\Models\Ussd\UssdSessionData::whereSessionId($r->sessionID)->wherePhoneNumber($r->phoneNumber)->first();
+            $sessionData = \App\Models\Ussd\UssdSessionData::whereSessionId($r->session_id)->wherePhoneNumber($r->phone_number)->first();
 
             // Create a new Subscription record using the subscription_data array and assign it to $subscription variable.
-            echo $r->insurance_type;
-
             if ($r->insurance_type == "crop" || $r->insurance_type == null) {
                 if ($sessionData != null) {
                     // Get the payment API for the subscriber's phone number.
