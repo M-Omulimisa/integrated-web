00448995718b5e7a633cfa45492265c5912a0e3c
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-07-24 10:02:00 +0300

    as

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -628,7 +628,7 @@ class Utils
             $farmer->year_of_birth = $ext['year_of_birth'];
             $farmer->created_at = $ext['created_at'];
             $farmer->marital_status = $ext['marital_status'];
-            $farmer->pwd_status = $ext['pwd_status']; 
+            $farmer->is_pwd = $ext['pwd_status']; 
             $farmer->home_gps_latitude = $ext['gps_latitude'];
             $farmer->home_gps_longitude = $ext['gps_longitude'];
 
