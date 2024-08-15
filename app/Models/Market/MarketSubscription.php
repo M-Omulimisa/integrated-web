8d688cdaf6c04ba3a5062604456d1d212f430620
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-08-15 05:24:40 +0300

    feat: Add welcome message functionality to MarketSubscription

--- app/Models/Market/MarketSubscription.php
+++ app/Models/Market/MarketSubscription.php
@@ -78,14 +78,14 @@ class MarketSubscription extends BaseModel
                 ->orWhere('recipient', $model->phone)
                 ->update([
                     'organization_id' => $model->organization_id
-                ]); 
+                ]);
         });
     }
 
     //prepre
     public static function send_welcome_message($model)
     {
-        /*  if($model->welcome_msg_sent != "Yes"){
+        if ($model->welcome_msg_sent != "Yes") {
             $phone = Utils::prepare_phone_number($model->phone);
             $msg = "Welcome to M-Omulimisa market information updates. You have successfully subscribed to {$model->package->name}. You will now receive regular market updates. Thank you for subscribing.";
 
@@ -111,7 +111,7 @@ class MarketSubscription extends BaseModel
                 $model->welcome_msg_sent_at = Carbon::now();
                 $model->welcome_msg_sent_details = 'Failed to send message to ' . $phone . ', Because: ' . $th->getMessage();
             }
-        } */
+        }
     }
 
     public static function prepare($m)
@@ -468,6 +468,7 @@ class MarketSubscription extends BaseModel
                                     ]);
                                 }
 
+                                sleep(10); //sleep for 10 seconds
                                 Utils::send_sms($recipient, $outbox->message);
                                 //message
                                 $this->welcome_msg_sent_details = "MARKET UPDATE: " . $outbox->message . ', WELCOME MESSAGE: ' . $this->welcome_msg_sent_details;
