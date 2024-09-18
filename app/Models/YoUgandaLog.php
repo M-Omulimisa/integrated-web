ed096403d6286758266b83e3501b40d459098dc6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-09-18 09:13:20 +0300

    added instant subscription on market module

--- app/Models/YoUgandaLog.php
+++ app/Models/YoUgandaLog.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use App\Models\Market\MarketSubscription;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class YoUgandaLog extends Model
 {
     use HasFactory;
+
+    //boot created
+    protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($model) {
+            //get market 
+            $market_sub = MarketSubscription::where(['payment_reference_id' => $model->external_ref])->first();
+            if ($market_sub != null) {
+                $market_sub->check_payment_status();
+                $market_sub = MarketSubscription::find($market_sub->id);
+                if ($market_sub != null) {
+                    if ($market_sub->is_paid == 'PAID') {
+                        $market_sub->send_renew_message();
+                    }
+                }
+            }
+        });
+    }
 }
