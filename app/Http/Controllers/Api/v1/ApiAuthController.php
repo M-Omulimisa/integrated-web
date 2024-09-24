d239b299a566e162c5ce93a5d436de35f6823f01
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-09-24 17:21:41 +0300

    Refactor ApiAuthController to use the user's organisation_id for limiting the number of farmers returned

--- app/Http/Controllers/Api/v1/ApiAuthController.php
+++ app/Http/Controllers/Api/v1/ApiAuthController.php
@@ -321,7 +321,7 @@ class ApiAuthController extends Controller
         if (!$u->isRole('admin')) {
             // $conditions = ['created_by_user_id' => $u->id];
         }
-        $conditions = ['organisation_id' => $u->id];
+        $conditions = ['organisation_id' => $u->organisation_id];
         return $this->success(Farmer::where($conditions)
             ->orderBy('created_at', 'desc')
             ->limit(150)
